8f76cd19e9833bb9b7fc64a9daf0bac080dbe607
Shibo Xing <shx26@pitt.edu>
2022-03-23 00:21:28 -0400

    fix: write back renamining to ins_q

--- project/components/decoder.cpp
+++ project/components/decoder.cpp
@@ -1,7 +1,6 @@
 #include "components.h"
 
-decoder::decoder(ins_table &ins_tb_ref) : ins_tb(ins_tb_ref){
-    
+decoder::decoder(ins_queue &ins_tb_ref) : ins_tb(ins_tb_ref){
     for (int i=31; i>=0; i--) {
        _free_lst.push_back(i);
     }
@@ -9,36 +8,37 @@ decoder::decoder(ins_table &ins_tb_ref) : ins_tb(ins_tb_ref){
 
 void decoder::rename_last() {
     // get the last register
-    auto last_ins = ins_tb.ins_q.front().second._info;
+    auto last_ins = &(ins_tb.ins_q.front().second._info);
+    string op = (*last_ins)[0];
 
     // iterate the instruction to check how many new registers are needed
-    vector<string> n_regs;
-    for (int i=1; i<4; i++) {
-        string reg_str = last_ins[i];
-        if ((reg_str[0] == 'R' || reg_str[0] == 'F') && _reg_lst.find(reg_str) == _reg_lst.end())
-            n_regs.push_back(reg_str);
+    vector<string> n_regs(4);
+    short reg_needed = 0;
+   
+    for (int i=3; i>=0; i--) { 
+        /* perform renaming backwards,
+        consider: F0, F0, F4 */
+        string reg_str = (*last_ins)[i];
+        
+        bool is_reg = (reg_str[0] == 'R' || reg_str[0] == 'F');
+
+        if ((i == 1 && op != "fsd" && op != "bne") || //always use a new register for DEST to eliminate false hazards
+                (is_reg && _reg_lst.find(reg_str) == _reg_lst.end())) { // use a new reg, is not mapped
+            
+            if (_free_lst.size() == 0) // not enough free regs, stall!
+                return;
+
+            n_regs[i] =  "p" + to_string(_free_lst.back()); // collect all unnamed regs
+            _free_lst.pop_back();
+        } else if (is_reg) // reg, but already has a mapping
+            n_regs[i] = _reg_lst[reg_str];
     }
 
-    // not enough free regs
-    if (n_regs.size() > _free_lst.size()) 
-        return;
-    else {
-        for (auto reg_str : n_regs) {
-            _reg_lst[reg_str] = "p" + to_string(_free_lst.back());
-            _free_lst.pop_back();
-        }
-    }
-
-    // last ins's dest reg
-    string dest = last_ins[1];
-    // iterate backwards to check for fake data hazards
-    for (int i=1; i<ins_tb.ins_q.size(); i++) {
-        auto curr = ins_tb.ins_q[i].second._info;
-        if (last_ins[0] != "fsd" && last_ins[0] != "bne" && dest[0] != '$' && (curr[1] == dest || curr[2] == dest || curr[3] == dest)){
-            // rename
-            _reg_lst[dest] = "p" + to_string(_free_lst.back());
-            _free_lst.pop_back();
-            break;
+    // execute the renaming, update the register mapping
+    for (int i=3; i>=0; i--) {
+        if (n_regs[i] != "") {
+            _reg_lst[string((*last_ins)[i])] = n_regs[i];
+            (*last_ins)[i] = n_regs[i];
         }
     }
 }
